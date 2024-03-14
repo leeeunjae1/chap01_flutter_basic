ff7e93a62c249a57636aec8a3ee7123563181941
이은재 <>
2024-03-14 10:49:39 +0900

    MaterialApp, Scaffold 구조

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,47 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'App 타이틀',
+      // App의 전반적인 테마를 설정한다.
+      theme: ThemeData(
+        textTheme: TextTheme(
+          bodyText1: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Appbar'),
+          backgroundColor: Colors.blue,
+        ),
+        body: Text('Body입니다.'),
+        // BottomNavigationBar
+        bottomNavigationBar: BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              label: 'business',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              label: 'school',
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.access_alarm),
+          onPressed: () => print("hello"),
+        ),
+      ),
+    );
   }
 }
