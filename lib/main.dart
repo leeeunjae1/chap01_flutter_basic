39597b311689b4ff535dd25d6ba39acfc4bf86dd
이은재 <>
2024-03-14 12:13:01 +0900

    main.dart update

--- lib/main.dart
+++ lib/main.dart
@@ -5,52 +5,69 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'App 타이틀',
-      // App의 전반적인 테마를 설정한다.
-      theme: ThemeData(
-        textTheme: TextTheme(
-          bodyText1: TextStyle(fontWeight: FontWeight.bold),
-        ),
-      ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Appbar'),
+          title: Text('AppBar'),
           backgroundColor: Colors.blue,
         ),
-
-        body: Container(
-          color: Colors.yellow,
+        body: SingleChildScrollView(
           child: Center(
-            child: Text("Body입니다."),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 100,
+                          color: Colors.blueGrey,
+                          child: Text('컨테이너'),
+                          alignment: Alignment.center,
+                          margin: EdgeInsets.all(20),
+                          padding: EdgeInsets.only(bottom: 20),
+                        ),
+                        Text(
+                          'hello',
+                          style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.amber),
+                        ),
+                      ],
+                    ),
+                    Icon(
+                      Icons.home,
+                      color: Colors.blueGrey,
+                      size: 100,
+                    ),
+                  ],
+                ),
+                Image.asset('assets/images/cat.jpg'),
+                Image.network(
+                    'https://gratisography.com/wp-content/uploads/2023/10/gratisography-cool-cat-800x525.jpg'),
+                Image.network(
+                    'https://gratisography.com/wp-content/uploads/2023/05/gratisography-colorful-cat-free-stock-photo-800x525.jpg'),
+                TextField(
+                  decoration: InputDecoration(labelText: 'Input'),
+                  // 입력 폼에 값이 변경될 경우 작동한다.
+                  onChanged: (text) {
+                    print(text);
+                  },
+                  // 엔터를 눌렀을 경우 작동한다.
+                  onSubmitted: (text) {
+                    print("enter를 눌렀습니다. 입력값 : $text");
+                  },
+                )
+              ],
+            ),
           ),
-        ),
-
-        // BottomNavigationBar
-        bottomNavigationBar: BottomNavigationBar(
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.business),
-              label: 'business',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.school),
-              label: 'school',
-            ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.access_alarm),
-          onPressed: () => print("hello"),
         ),
       ),
     );
